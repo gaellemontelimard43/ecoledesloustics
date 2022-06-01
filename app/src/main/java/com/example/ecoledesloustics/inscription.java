2a30cb4290b173738f03d69668b6b7d4bb032d11
montelig <g.montelimard43@gmail.com>
2022-06-01 15:51:45 +0200

    debut3

--- app/src/main/java/com/example/ecoledesloustics/inscription.java
+++ app/src/main/java/com/example/ecoledesloustics/inscription.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class win extends AppCompatActivity {
+public class inscription extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.win);
+        setContentView(R.layout.inscription);
     }
 
 }
---
+++ app/src/main/java/com/example/ecoledesloustics/perdu.java
@@ -0,0 +1,15 @@
+package com.example.ecoledesloustics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class perdu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.perdu);
+    }
+
+}
