12ed9c0edb401198aa6a538c9040a7f8f00974cf
MalikaJaghdam <molkajaghdam@gmail.com>
2022-06-02 11:50:39 +0200

    yooooo

--- app/src/main/java/com/example/ecoledesloustics/perdu.java
+++ app/src/main/java/com/example/ecoledesloustics/perdu.java
@@ -5,11 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class perdu extends AppCompatActivity {
+    private UserModel userModel;
+    private MathsCategoryModel mathCatModel;
+    private int erreurs;
+    private boolean timeOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.perdu);
+        if (getIntent().getExtras() != null){
+            userModel= getIntent().getParcelableExtra("user");
+            mathCatModel = getIntent().getParcelableExtra(math_cat);
+            errors = getIntent().getIntExtra("errors", 0);
+            timeOut = getIntent().getBooleanExtra("timeOut", true);
+        }
+        TextView
     }
 
 }
