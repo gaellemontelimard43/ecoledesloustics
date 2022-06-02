5b9005d85855daa206870b5bdabcb615e40859d3
montelig <g.montelimard43@gmail.com>
2022-06-02 11:01:17 +0200

    pio

--- app/src/main/java/com/example/ecoledesloustics/db/userModel.java
+++ app/src/main/java/com/example/ecoledesloustics/db/userModel.java
@@ -1,4 +1,74 @@
 package com.example.ecoledesloustics.db;
 
-public class userModel {
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+@Entity
+public class userModel implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "login")
+    private String login;
+
+    @ColumnInfo(name = "prenom")
+    private String prenom;
+
+    @ColumnInfo(name = "nom")
+    private String nom;
+
+    @ColumnInfo(name = "email")
+    private String email;
+
+    @ColumnInfo(name = "motdepasse")
+    private String motdepasse;
+
+    /*
+     * Getters and Setters
+     * */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getprenom() {
+        return prenom;
+    }
+
+    public void setprenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getnom() {
+        return nom;
+    }
+
+    public void setnom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getemail() {
+        return email;
+    }
+
+    public void setemail(String email) {
+        this.email = email;
+    }
+
+    public String getmotdepasse() {
+        return motdepasse;
+    }
+
+    public void setmotdepasse(String motdepasse) {
+        this.motdepasse = motdepasse;
+    }
+
 }
