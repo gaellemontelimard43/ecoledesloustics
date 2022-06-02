8b36c4ffb4c1c1e3c90cb9e1887543a6fc68004b
MalikaJaghdam <molkajaghdam@gmail.com>
2022-06-02 11:00:46 +0200

    yoooo

--- app/src/main/java/com/example/ecoledesloustics/db/userDAO.java
+++ app/src/main/java/com/example/ecoledesloustics/db/userDAO.java
@@ -1,4 +1,31 @@
 package com.example.ecoledesloustics.db;
 
-public class userDAO {
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+
+    @Query("SELECT * FROM user")
+    List<UserModel> getAll();
+
+    @Query("SELECT * FROM user WHERE prenom= :prenom AND nom = :nom")
+
+    @Insert
+    long insert(UserModel user);
+
+    @Insert
+    long[] insertAll(UserModel...users);
+
+    @Delete
+    void delete(UserModel user);
+
+    @Update (onConflict = OnConflictStrategy.REPLACE)
+    void update(Task task);
+
 }
